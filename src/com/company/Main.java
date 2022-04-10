93db669ee4c92084159e0004d794ee8282d49b5d
KennyWagner <wagnerkenneth72@gmail.com>
2022-04-10 16:31:16 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	int summerCost = 230;
+    int fallCost = 180;
+    int winterCost = 60;
+    int springCost = 450;
+
+
+    System.out.println("Your yearly maintenance cost is " + (summerCost + fallCost + winterCost + springCost) + "$");
+    }
+}
